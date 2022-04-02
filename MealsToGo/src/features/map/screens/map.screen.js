7b673377075bca4141ffbb6e19289e35b3aeca38
MariosKampouris <marioswow20@gmail.com>
2022-04-02 13:07:54 -0400

    Introduced Map Markers (Pins)

--- MealsToGo/src/features/map/screens/map.screen.js
+++ MealsToGo/src/features/map/screens/map.screen.js
@@ -36,12 +36,22 @@ export const MapScreen = () => {
                     longitudeDelta: 0.02,
                 }}>
                 {restaurants.map((restaurant) => {
-                    return null;
+                    return (
+                    <MapView.Marker
+                        key={restaurant.name}
+                        title={restaurant.name}
+                        coordinate={{
+                            latitude: restaurant.geometry.location.lat,
+                            longitude: restaurant.geometry.location.lng,
+                        }}
+                    />
+
+                    );
                 })}
             </MapView>
         </>
-    )
-}
+    );
+};
 
 
 const styles = StyleSheet.create({
