725605de4876bf9ca26f819c65adb648cb398c5a
MariosKampouris <marioswow20@gmail.com>
2022-05-15 20:25:14 -0400

    Fully integrated Google Geocoding and Places API

--- MealsToGo/functions/geocode/index.js
+++ MealsToGo/functions/geocode/index.js
@@ -15,10 +15,10 @@ module.exports.geocodeRequest = (request, response, client) => {
             key: functions.config().google.key,
         },
         timeout: 1000,
-    }).then(res => {
+    }).then((res) => {
         return response.json(res.data);
-    }).catch(e => {
+    }).catch((e) => {
         response.status(400);
         return response.send(e.response.data.error_message);
-    })
-}
+    });
+};
